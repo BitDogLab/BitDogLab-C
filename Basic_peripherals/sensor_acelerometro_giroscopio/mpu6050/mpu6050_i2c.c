d7845c0eaf4b490c869cd8cedd3fb9acb7a6518c
Fábio Henrique M Oliveira <oliveirafhm@Fabios-MacBook-Pro-2.local>
2025-07-11 23:33:05 -0300

    Incremento com funções para leitura e configuração da sensibilidade do acelerômetro do MPU6050 e conversão de dados.

--- Basic_peripherals/sensor_acelerometro_giroscopio/mpu6050/mpu6050_i2c.c
+++ Basic_peripherals/sensor_acelerometro_giroscopio/mpu6050/mpu6050_i2c.c
@@ -22,6 +22,22 @@ void mpu6050_reset() {
     sleep_ms(10);
 }
 
+uint8_t mpu6050_get_accel_range() {
+    uint8_t reg = 0x1C;
+    uint8_t val;
+    i2c_write_blocking(I2C_PORT, MPU6050_ADDR, &reg, 1, true);
+    i2c_read_blocking(I2C_PORT, MPU6050_ADDR, &val, 1, false);
+    return (val >> 3) & 0x03; // bits 4:3
+}
+
+// 0=±2g, 1=±4g, 2=±8g, 3=±16g
+void mpu6050_set_accel_range(uint8_t range) {
+    uint8_t buf[2];
+    buf[0] = 0x1C; // ACCEL_CONFIG register
+    buf[1] = range << 3; // bits 3 e 4
+    i2c_write_blocking(I2C_PORT, MPU6050_ADDR, buf, 2, false);
+}
+
 void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
     uint8_t buffer[6];
     uint8_t reg = 0x3B; //MPU6050_REG_ACCEL_XOUT_H
